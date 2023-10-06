fa68d240987d7c2e6b2911c51f31dc36d657f0a4
Руслан Захаров <rsln.zakharov@yandex.ru>
2023-10-07 04:00:19 +0900

    Диплом1_Ревъю_0

---
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/service/StatsServiceImpl.java
@@ -0,0 +1,41 @@
+package ru.practicum.ewm.stats.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.practicum.ewm.stats.dto.EndpointHitDto;
+import ru.practicum.ewm.stats.dto.ViewStatsDto;
+import ru.practicum.ewm.stats.mapper.EndpointHitMapper;
+import ru.practicum.ewm.stats.mapper.ViewStatsMapper;
+import ru.practicum.ewm.stats.model.EndpointHit;
+import ru.practicum.ewm.stats.model.ViewStats;
+import ru.practicum.ewm.stats.repository.StatsRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class StatsServiceImpl implements StatsService {
+    private final StatsRepository statsRepository;
+
+    @Override
+    public void save(EndpointHitDto endpointHitDto) {
+        EndpointHit createdEndpointHit = statsRepository.save(EndpointHitMapper.toEndpointHit(endpointHitDto));
+        log.info("Добавлена статистика c id{}", createdEndpointHit.getId());
+    }
+
+    @Override
+    public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        List<ViewStats> allViewStats;
+        if (unique) {
+            allViewStats = statsRepository.getByUniqueIp(start, end, uris);
+        } else {
+            allViewStats = statsRepository.getEndpointHit(start, end, uris);
+        }
+        log.info("Получено статистики: {}", allViewStats.size());
+        return allViewStats.stream().map(ViewStatsMapper::toViewStatsDto).collect(Collectors.toList());
+    }
+}
