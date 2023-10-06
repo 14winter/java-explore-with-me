c4d94d2772f94deed657e1703ae81b648e28e05e
Руслан Захаров <rsln.zakharov@yandex.ru>
2023-10-07 04:00:19 +0900

    Диплом1_Ревъю_0

---
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/EndpointHitDto.java
@@ -0,0 +1,21 @@
+package ru.practicum.ewm.stats.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EndpointHitDto {
+    private String app;
+    private String uri;
+    private String ip;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
