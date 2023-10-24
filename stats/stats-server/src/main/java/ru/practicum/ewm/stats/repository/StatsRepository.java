63f1b5a6c0733d4dd24988837263cd8d4deaaa73
Руслан Захаров <rsln.zakharov@yandex.ru>
2023-10-25 02:48:44 +0900

    Диплом2_1_Ревью_1

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/repository/StatsRepository.java
@@ -13,8 +13,7 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
     @Query("SELECT new ru.practicum.ewm.stats.model.ViewStats(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
             "FROM EndpointHit AS e " +
             "WHERE e.timestamp BETWEEN :start AND :end " +
-            "AND ((:uris, NULL) is NULL or e.uri in :uris) " +
-            // "AND (COALESCE(:uris, NULL) is NULL or e.uri in :uris) " +
+            "AND (COALESCE(:uris, NULL) is NULL or e.uri in :uris) " +
             "GROUP BY e.app, e.uri " +
             "ORDER BY COUNT(e.ip) DESC ")
     List<ViewStats> getByUniqueIp(@Param("start") LocalDateTime start,
@@ -24,8 +23,7 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
     @Query("SELECT new ru.practicum.ewm.stats.model.ViewStats(e.app, e.uri, COUNT(e.ip)) " +
             "FROM EndpointHit AS e " +
             "WHERE e.timestamp BETWEEN :start AND :end " +
-            "AND ((:uris, NULL) is NULL or e.uri in :uris) " +
-            //"AND (COALESCE(:uris, NULL) is NULL or e.uri in :uris) " +
+            "AND (COALESCE(:uris, NULL) is NULL or e.uri in :uris) " +
             "GROUP BY e.app, e.uri " +
             "ORDER BY COUNT(e.ip) DESC ")
     List<ViewStats> getEndpointHit(@Param("start") LocalDateTime start,
